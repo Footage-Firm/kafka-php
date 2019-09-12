a0e8bab5a2b7b5f7e37ab0b0ec340dc345884f7c
Brendan Alexander <brendan@videoblocks.com>
2019-09-12 15:53:52 -0400

    simplify fake records

--- Tests/Fakes/FakeFactory.php
+++ Tests/Fakes/FakeFactory.php
@@ -7,23 +7,11 @@ use Faker\Factory;
 class FakeFactory
 {
 
-    public static function fakeRecord(int $id = null, string $name = null): FakeRecord
+    public static function fakeRecord(int $id = null): FakeRecord
     {
         $id = $id ?? Factory::create()->numberBetween(0, 9999);
-        $name = $name ?? Factory::create()->word;
         $fake = new FakeRecord();
         $fake->setId($id);
-        $fake->setName($name);
-        return $fake;
-    }
-
-    public static function fakeRecordTwo(int $id = null, string $name = null): FakeRecordTwo
-    {
-        $id = $id ?? Factory::create()->numberBetween(0, 9999);
-        $name = $name ?? Factory::create()->word;
-        $fake = new FakeRecordTwo();
-        $fake->setId($id);
-        $fake->setName($name);
         return $fake;
     }
 }
