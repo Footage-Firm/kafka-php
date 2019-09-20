107a6d7b4bff7bd4944e4859a5ad744be442559e
chasdevs <chas@storyblocks.com>
2019-09-20 12:54:01 -0400

    Add faker getter and small test for producing a record.

--- Tests/Producer/Integration/TestProducer.php
+++ Tests/Producer/Integration/TestProducer.php
@@ -3,27 +3,35 @@
 namespace Test\Producer\Integration;
 
 use App\Producer\ProducerBuilder;
+use EventsPhp\Storyblocks\Common\DebugRecord;
 use Exception;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 use Tests\Fakes\FakeFactory;
 use Tests\Fakes\FakeRecord;
-use Tests\WithFaker;
 
 class TestProducer extends TestCase
 {
 
-    use WithFaker;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->initFaker();
-    }
-
     private $schemaRegistryUrl = "http://0.0.0.0:8081";
 
     private $brokers = ["0.0.0.0:29092"];
+
+    public function setUp(): void
+    {
+        $this->brokers = getenv('KAFKA_URL') ? [getenv('KAFKA_URL')] : $this->brokers;
+        $this->schemaRegistryUrl = getenv('SCHEMA_REGISTRY_URL') ?: $this->schemaRegistryUrl;
+    }
+
+    public function testProduceRecord()
+    {
+        $this->expectNotToPerformAssertions();
+        $builder = new ProducerBuilder($this->brokers, $this->schemaRegistryUrl);
+        $producer = $builder->build();
+        $record = new DebugRecord();
+        $record->setPayload("kafka-php unit test");
+        $producer->produce($record);
+    }
 
     public function testExceptionThrown_WhenFailsToEncode()
     {
--- Tests/WithFaker.php
+++ Tests/WithFaker.php
@@ -11,9 +11,20 @@ trait WithFaker
     /** @var Generator */
     protected $faker;
 
+    /**
+     * @deprecated Use the singleton getter below.
+     */
     public function initFaker(): void
     {
         $this->faker = Factory::create();
+    }
+
+    public function faker(): Generator
+    {
+        if (!$this->faker) {
+            $this->faker = Factory::create();
+        }
+        return $this->faker;
     }
 }
 
